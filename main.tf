a79ce0c3da308efd11f1b8eae9a964c3111c6397
Raghu K <raghuk.vit@gmail.com>
2022-06-08 08:19:06 +0530

    Update roboshop-infra/terraform-vpc-transit-setup/

--- main.tf
+++ main.tf
@@ -44,10 +44,15 @@ variable "subnets" {
   default = ["172.31.96.0/20", "172.31.112.0/20", "172.31.128.0/20", "172.31.144.0/20", "172.31.160.0/20", "172.31.176.0/20"]
 }
 
+variable "AZ" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
+}
+
 resource "aws_subnet" "private" {
-  count      = length(var.subnets)
-  vpc_id     = var.DEFAULT_VPC_ID
-  cidr_block = element(var.subnets, count.index)
+  count             = length(var.subnets)
+  vpc_id            = var.DEFAULT_VPC_ID
+  cidr_block        = element(var.subnets, count.index)
+  availability_zone = element(var.AZ, count.index)
   tags = {
     Name = "default-vpc-private-${count.index + 1}"
   }
