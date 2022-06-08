c545149c07b400bd8412ca14d94f96c57f863111
Raghu K <raghuk.vit@gmail.com>
2022-06-08 08:10:43 +0530

    Update roboshop-infra/terraform-vpc-transit-setup/

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_eip" "ngw" {
 
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.ngw.id
-  subnet_id     = data.aws_subnet_ids.default-vpc.ids[0]
+  subnet_id     = "subnet-03e3869fb4143cbc1"
 
   tags = {
     Name = "NGW"
