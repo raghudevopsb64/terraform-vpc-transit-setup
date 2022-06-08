01defe06d8462d8eb0df42cc3170fd2b039bc117
Raghu K <raghuk.vit@gmail.com>
2022-06-08 08:27:28 +0530

    Update roboshop-infra/terraform-vpc-transit-setup/

--- main.tf
+++ main.tf
@@ -80,9 +80,11 @@ resource "aws_route_table_association" "private" {
 
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-attach" {
-  subnet_ids         = aws_subnet.private.*.id
-  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-  vpc_id             = var.DEFAULT_VPC_ID
+  subnet_ids                                      = aws_subnet.private.*.id
+  transit_gateway_id                              = aws_ec2_transit_gateway.tgw.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  vpc_id                                          = var.DEFAULT_VPC_ID
   tags = {
     Name = "roboshop-tgw-attach-${var.ENV}"
   }
