08abf10ec600424a6ad2adb3e57e995a3d930f39
Raghu K <raghuk.vit@gmail.com>
2022-06-09 08:26:15 +0530

    Update roboshop-infra/terraform-vpc-transit-setup/

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,28 @@
-output "TRANSIT_GW" {
-  value = aws_ec2_transit_gateway.tgw.id
-}
+//output "TRANSIT_GW" {
+//  value = aws_ec2_transit_gateway.tgw.id
+//}
+//
+//output "DEFAULT_VPC_ID" {
+//  value = var.DEFAULT_VPC_ID
+//}
+//
+//output "DEFAULT_VPC_RT" {
+//  value = var.DEFAULT_VPC_RT
+//}
+//
+//output "DEFAULT_VPC_CIDR" {
+//  value = data.aws_vpc.default.cidr_block
+//}
+//
+//output "DEFAULT_VPC_TRANSIT_GW_ATTACHMENT" {
+//  value = aws_ec2_transit_gateway_vpc_attachment.tgw-attach.id
+//}
+//
+//output "DEFAULT_VPC_TRANSIT_GW_ROUTE_TABLE" {
+//  value = aws_ec2_transit_gateway_route_table.default-vpc.id
+//}
 
-output "DEFAULT_VPC_ID" {
-  value = var.DEFAULT_VPC_ID
-}
 
-output "DEFAULT_VPC_RT" {
-  value = var.DEFAULT_VPC_RT
-}
-
-output "DEFAULT_VPC_CIDR" {
-  value = data.aws_vpc.default.cidr_block
-}
-
-output "DEFAULT_VPC_TRANSIT_GW_ATTACHMENT" {
-  value = aws_ec2_transit_gateway_vpc_attachment.tgw-attach.id
-}
-
-output "DEFAULT_VPC_TRANSIT_GW_ROUTE_TABLE" {
-  value = aws_ec2_transit_gateway_route_table.default-vpc.id
+output "SUBNETS" {
+  value = data.aws_subnet_ids.default-vpc
 }
