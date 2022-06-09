0adf409296444fad601a5724787ac7eb4705dbbf
Raghu K <raghuk.vit@gmail.com>
2022-06-09 09:06:27 +0530

    Update roboshop-infra/terraform-vpc-transit-setup/

--- data.tf
+++ data.tf
@@ -1,10 +1,12 @@
-data "aws_subnet_ids" "default-vpc" {
-  vpc_id = var.DEFAULT_VPC_ID
-}
-
 data "aws_subnets" "ngw" {
   filter {
     name   = "tag:NGW"
     values = ["true"]
   }
 }
+
+data "aws_vpc" "default" {
+  id = var.DEFAULT_VPC_ID
+}
+
+
