17bbfcbb3be858fe3a23e574ac62a565a1ad57c9
Raghu K <raghuk.vit@gmail.com>
2022-06-09 08:24:37 +0530

    Update roboshop-infra/terraform-vpc-transit-setup/

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-//resource "aws_ec2_transit_gateway" "tgw" {
-//  description = "roboshop-tgw"
-//  tags = {
-//    Name = "roboshop-tgw-${var.ENV}"
-//  }
-//}
-//
-//resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-attach" {
-//  subnet_ids         = data.aws_subnet_ids.default-vpc.ids
-//  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-//  vpc_id             = var.DEFAULT_VPC_ID
-//  tags = {
-//    Name = "roboshop-tgw-attach-${var.ENV}"
-//  }
-//}
-//
-
-
-////////////////
-
 resource "aws_ec2_transit_gateway" "tgw" {
   description                     = "roboshop-tgw"
   default_route_table_association = "disable"
@@ -33,7 +13,7 @@ resource "aws_eip" "ngw" {
 
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.ngw.id
-  subnet_id     = "subnet-03e3869fb4143cbc1"
+  subnet_id     = data.aws_subnet_ids.default-vpc.ids[0]
 
   tags = {
     Name = "NGW"
