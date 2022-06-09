52534553031295ce35c9fb7ac4717c44a17e22c7
Raghu K <raghuk.vit@gmail.com>
2022-06-09 08:09:03 +0530

    Update roboshop-infra/terraform-vpc-transit-setup/

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,7 @@ output "DEFAULT_VPC_CIDR" {
   value = data.aws_vpc.default.cidr_block
 }
 
+output "DEFAULT_VPC_TRANSIT_GW_ATTACHMENT" {
+  value = aws_ec2_transit_gateway_vpc_attachment.tgw-attach.id
+}
+
